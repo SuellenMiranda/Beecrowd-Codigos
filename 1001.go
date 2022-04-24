6f1364c49a3ea59e242b3118341828eba5c76789
Suh <suellen.org@gmail.com>
2022-04-23 23:37:26 -0300

    Create 1001.go

---
+++ 1001.go
@@ -0,0 +1,19 @@
+//Extremamente fácil
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+  
+  var A, B int
+  
+  fmt.Scanf("%d\n%d\n", &A, &B)
+  
+  X := A + B
+  
+  fmt.Println("X =",X)
+  
+}
